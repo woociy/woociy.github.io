0d1b7b014f8b65bc0073237de8dc2946b0b03420
woociy <954466362@qq.com>
2016-10-27 21:42:37 +0800

    期中匯報
    期中匯報準備

--- js/js002.js
+++ js/js002.js
@@ -1,12 +1,6 @@
-function sayHello() {
-    //console.log("Hello");
-
-    // 取得 input 的值
-    var str = $("#input").val();
-
-    // 將 str 設定到 output 
+function sayhello(){
+    var str =$("#input").val();
     $("#output").text(str);
-}
 
-// 將 id名為 test 的 button，綁定 click 事件 
-$("#test").bind('click', sayHello);
+}
+$("#text").bind('click',sayhello);
