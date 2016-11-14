e72caa66ffa1c71140533197fade079740e24cad
woociy <954466362@qq.com>
2016-11-14 15:34:33 +0800

    种苹果上课修改版1.2

--- js/d3_01.js
+++ js/d3_01.js
@@ -1,3 +1,4 @@
+
 var svgctrl = d3.select('svg');
 var c1 = svgctrl.append('circle');
 c1.attr('cx', 300).attr('cy', 100).attr('r', 10).attr('stroke-width', 3).attr('fill', 'red');
@@ -5,10 +6,6 @@ c1.attr('cx', 300).attr('cy', 100).attr('r', 10).attr('stroke-width', 3).attr('f
 function move() {
     var svgctrl = d3.select('svg');
     var c2 = svgctrl.append('circle');
-    // var x1 = Math.floor(Math.random() * 400);
-    // var y1 = Math.floor(Math.random() * 400);
-    // $('#input').text(x1);
-    // $('#input').text(y1);
 
     var x = $("#output_x").text();
     var y = $("#output_y").text(); //注意編碼怎麽寫 
@@ -16,13 +13,12 @@ function move() {
 }
 
 function movex() {
-    var x1 = Math.floor(Math.random() * 15)*22+160;
+    var x1 = Math.floor(Math.random() * 15)*22+175;
     $('#output_x').text(x1);
-
 }
 
 function movey() {
-    var y1 = Math.floor(Math.random() * 10)*16+110;
+    var y1 = Math.floor(Math.random() * 10)*16+112;
     $('#output_y').text(y1);
 }
 
